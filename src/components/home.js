783ca6736648aec9ac4a692660478a21ead45ed5
HarisHassan55 <harishassan551@gmail.com>
2024-08-18 20:26:22 +0500

    second commit

--- src/components/home.js
+++ src/components/home.js
@@ -1,30 +1,34 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import Calender from './calender';
-import Slot from './slot';
-import Table from './table';
+import React from "react";
+import { useSelector } from "react-redux";
+import Calender from "./calender";
+import Slot from "./slot";
+import Table from "./table";
 export default function Home() {
-  const { selectedDate, selectedSlot, selectedTable, userInfo } = useSelector((state) => state.app);
+  const { selectedDate, selectedSlot, selectedTable, userInfo } = useSelector(
+    (state) => state.app
+  );
 
   return (
-    <div className='flex flex-col gap-y-5 sm:justify-between sm:flex-row py-[2rem]  '>
-      <div className='flex flex-col gap-y-5 sm:flex-row sm:gap-x-3 items-center'>
-        <Calender />
-        <Slot />
-      </div>
+    <div className="flex flex-col">
+      <div className="flex flex-col gap-y-5 sm:justify-between sm:flex-row py-[2rem]  ">
+        <div className="flex flex-col gap-y-5 sm:flex-row sm:gap-x-3 items-center">
+          <Calender />
+          <Slot />
+        </div>
 
-      <div className='flex flex-row gap-x-[1rem] self-center'>
-        <div className='flex flex-col p-1'>
-          <div className='flex size-4 bg-gray-500 rounded-full self-center'></div>
-          <p className='text-center'>Reserved</p>
-        </div>
-        <div className='flex flex-col p-1'>
-          <div className='flex size-4 bg-neutral-50 rounded-full self-center'></div>
-          <p className='text-center'>Available</p>
-        </div>
-        <div className='flex flex-col p-1'>
-          <div className='flex size-4 bg-blue-500 rounded-full self-center'></div>
-          <p className='text-center'>Selected</p>
+        <div className="flex flex-row gap-x-[1rem] self-center">
+          <div className="flex flex-col p-1">
+            <div className="flex size-4 bg-gray-500 rounded-full self-center"></div>
+            <p className="text-center">Reserved</p>
+          </div>
+          <div className="flex flex-col p-1">
+            <div className="flex size-4 bg-neutral-50 rounded-full self-center"></div>
+            <p className="text-center">Available</p>
+          </div>
+          <div className="flex flex-col p-1">
+            <div className="flex size-4 bg-blue-500 rounded-full self-center"></div>
+            <p className="text-center">Selected</p>
+          </div>
         </div>
       </div>
 
